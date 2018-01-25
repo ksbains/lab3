64ea633cb9a762bf00eea066e6a8a81ef2e2d5ba
ksbains <k1bains@ucsd.edu>
2018-01-25 10:21:17 -0800

    got the heroku part of the lab and need to commit so heroku can deploy all of the changes i made

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,32 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Experience Chan");
 	});
+	$("#testjs").text("Please wait...")
+	//$("#testjs").toggleClass("active");
+	//$("#testjs").("active");
+
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	projectClick();
+}
+
+
+function projectClick (){
+	$(".project").click(function(e){
+		e.preventDefault();
+		var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+
+       //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+       description.fadeOut();
+    }
+});
+
+	
 }
